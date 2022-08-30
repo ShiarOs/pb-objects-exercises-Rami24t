08859454ed0454203fc74a4c2fa0f11d97f51cdb
Rami <103028944+Rami24t@users.noreply.github.com>
2022-08-30 19:47:25 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -10,10 +10,10 @@ function isPlainObject(object) {
     return !(object === null || object instanceof Array) && (object instanceof Object);
 }
 const data = { a: 1 };
-console.log(isPlainObject(data)); // -> true
-console.log(isPlainObject([1, 2, 3])); // -> false
-console.log(isPlainObject(null)); // ->false
-console.log(isPlainObject('Stiring')); // -> false
+console.log('1: ', isPlainObject(data)); // -> true
+console.log('1: ', isPlainObject([1, 2, 3])); // -> false
+console.log('1: ', isPlainObject(null)); // ->false
+console.log('1: ', isPlainObject('Stiring')); // -> false
 
 // //2
 // //write your code here
@@ -72,37 +72,95 @@ console.log('5:', isEqual(data53, data52)); // true
 //6
 const data6 = { a: { b: [1, 2, 3] } }
 
-function invoke(obj, str, fun, arr) {
-    const code = ('obj' + '.' + str + '.' + fun + '(' + arr + ');');
-    return eval(code);
+const invoke = (obj, str, fun, args) => eval('obj' + '.' + str + '.' + fun + '(' + args + ');');
+
+console.log('6: ', invoke(data6, 'a.b', 'splice', [1, 2])) // [2, 3]
+
+//7
+const data7 = { a: { b: undefined } };
+
+function isEmptyDeep(object) {
+    if (isEmpty(object))
+        return true
+    for (let value of Object.values(object)) {
+        if (isEmpty(value))
+            return true
+        for (const value2 of Object.values(value)) {
+            if (isEmpty(value2))
+                return true
+            for (let value3 of Object.values(value2)) {
+                if (isEmpty(value3))
+                    return true
+                for (const value4 of Object.values(value3)) {
+                    if (isEmpty(value4))
+                        return true
+                    for (let value5 of Object.values(value4)) {
+                        if (isEmpty(value5))
+                            return true
+                        for (const value6 of Object.values(value5)) {
+                            if (isEmpty(value6))
+                                return true
+                            for (let value7 of Object.values(value6)) {
+                                if (isEmpty(value7))
+                                    return true
+                                for (const value8 of Object.values(value7)) {
+                                    if (isEmpty(value8))
+                                        return true
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false
 }
-console.log(invoke(data6, 'a.b', 'splice', [1, 2])) // [2, 3]
+
+const data8a = { a: { b: { c: { d: { e: { f: { g: { h: { i: undefined } } } } } } } } };
+const data9a = { a: { b: { c: { d: { e: { f: { g: { h: { i: 99 } } } } } } } } };
+
+console.log('7:', isEmptyDeep(data7)); // -> true
+console.log('7:', isEmptyDeep(data8a)); // -> true
+console.log('7:', isEmptyDeep(data9a)); // -> true
+
+//8
+const data8 = { a: 1, b: { c: { d: 1 } } };
+const data81 = { a: 1, b: { c: { d: 1 } } };
+const data82 = { a: 1, b: { c: { d: 2 } } };
+
+function isEqualDeep(a, b) {
+    return isEqual(a, b);
+}
+
+console.log('8: ', isEqualDeep(data8, data81));// true
+console.log('8: ', isEqualDeep(data8, data82)); // false
 
 
-// //7
-// const data7 = { a: { b: undefined } };
-// //write your code here 
-// console.log(isEmptyDeep(data7));
+//9
+const data9 = { a: 1, b: 2 };
+const data91 = { c: 1, b: 2 };
+//write your code here
+
+function intersection(a, b2) {
+    a = Object.entries(a)
+    b2 = Object.entries(b2)
+    for (const ae of a) {
+        if (b2.findIndex(b2e => isEqual(b2e, ae)) > -1)
+            return Object.fromEntries([ae]);
+    }
+}
+
+console.log('9', intersection(data9, data91)); // { b: 2 }
 
 
-// //8
-// const data8 = { a: 1, b: { c: 1 } };  
-// const data81= { a: 1, b: { c: 1 } };  
-// const data82= { a: 1, b: { c: 2 } };
-// //write your code here 
-// console.log(isEqualDeep(data8, data81));// true  
-// console.log(isEqualDeep(data8, data82)); // false  
+//10
+const data10 = { a: 1, b: { c: 3 } };
+const data11 = { c: 1, b: { c: 3 } };
+const data12 = { a: 1, b: { c: { d: 3 } } };
+const data13 = { c: 1, b: { c: { d: 3 } } };
 
+const intersectionDeep = intersection;
 
-// //9
-// const data9 = { a: 1, b: 2 };  
-// const data91 = { c: 1, b: 2 };
-// //write your code here 
-// console.log(intersection(data9, data91)); // { b: 2 }
-
-
-// //10
-// const data10 = { a: 1, b: { c: 3 } };  
-// const data11 = { c: 1, b: { c: 3 } };
-// //write your code here 
-// console.log(intersectionDeep(data10, data11)); // { b: { c: 3 } }
+console.log('10:', intersectionDeep(data10, data11)); // { b: { c: 3 } }
+console.log('10:', intersectionDeep(data12, data13)); // 10: { b: { c: { d: 3 } } }
